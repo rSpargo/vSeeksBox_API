00d3af497234426f21b3f5605bd063a3ebd72498
Ryan Spargo <ryanspargo@gmail.com>
2017-10-18 15:22:06 -0600

    Fixed typo

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ app.get('/', function(req, res) {
     res.send("Welcome to the API.");
 });
 app.get('/saveUser', urlEncodedParser, api.postUser);
-app.post('/saveVSeeks:user', urlEncodedParser, api.postVSeeks);
+app.post('/saveVSeeks/:user', urlEncodedParser, api.postVSeeks);
 
 app.listen(process.env.PORT || 5000);
