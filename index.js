d61a3217cdcec9ba112def917e5f3a9402b8e7ba
Ryan Spargo <ryanspargo@gmail.com>
2017-10-19 18:35:41 -0600

    changed /saveUser to POST instead of GET.

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ bodyParser = require('body-parser');
 app = express();
 var urlEncodedParser = bodyParser.urlencoded({ extended: true });
 
+/* app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+ */
 //app.get('/getUsers', api.getUsers);
 app.get('/', function(req, res) {
     res.send("Welcome to the API.");
 });
-app.get('/saveUser', urlEncodedParser, api.postUser);
+app.post('/saveUser', urlEncodedParser, api.postUser);
 app.post('/saveVSeeks/:user', urlEncodedParser, api.postVSeeks);
 
 app.listen(process.env.PORT || 5000);
