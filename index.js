4367a249d15e770c7236755f8d9179aa9a3adfaa
Ryan Spargo <ryanspargo@gmail.com>
2018-01-26 08:19:00 -0700

    Removed all functions except genID for now. This API has become obselete.

--- index.js
+++ index.js
@@ -13,17 +13,9 @@ app.use(function(req, res, next) {
     next();
   });
 
-//app.get('/getUsers', api.getUsers);
 app.get('/', function(req, res) {
     res.send("Welcome to the API.");
 });
-app.post('/saveUser', api.postUser);
-app.post('/saveVSeeks/:user', api.postVSeeks);
-app.post('/savePrefs/:user', api.postPrefs);
-app.post('/saveData/:user', api.postData);
-
-app.get('/getVSeeks/:user', api.getVSeeks);
-app.get('/getData/:user', api.getData);
 app.get('/genID', api.genID);
 
 app.listen(process.env.PORT || 5000);
